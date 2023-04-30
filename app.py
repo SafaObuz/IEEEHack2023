54717815c0b4dcfc613ba95a63f4da542094917d
testsafa <test@gmail.com>
2023-04-30 02:15:56 -0400

    Trying to guide the prompt

--- app.py
+++ app.py
@@ -15,7 +15,20 @@ def question():
         form_values = request.form.items()
         response_string = ", ".join([f"{k}: {v}" for k, v in form_values])
 
-        prompt = f'{response_string}. How would you recommend reducing your emissions?'
+        print(response_string)
+
+        prompt = f'''
+        {response_string}
+        Given these responses, how should this person reduce their CO2 emmissions?
+        Use these examples as guidelines for your prompt. Follow the relationships formed below.
+
+        Do you eat meat?: Y--> Meat products and dairy contribute twice as much as plant products to emmisions according to a study.
+        Do you eat meat?: Y--> Meat accounts for 14.5% of global emmissions according to the UN
+        Do you eat meat?: N--> Plant-based products help keep your emmissions low. Continue to eat plant based products if you wish to keep your CO2 emmissions low.
+
+        What's your monthly electric bill?: 0--> 
+        What's your monthly electric bill?: 10000000--> Because your monthly electic bills are so high, I reccomend that you look into decreasing your energy consumption.
+        '''
 
         response = openai.Completion.create(
              model="text-davinci-003",
