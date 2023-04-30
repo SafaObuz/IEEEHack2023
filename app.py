3448e9232a3b9826a3110f519c1698f35562f21c
testsafa <test@gmail.com>
2023-04-30 01:24:56 -0400

    now allows for variable number of responses

--- app.py
+++ app.py
@@ -1,18 +1,8 @@
 import openai
 from flask import Flask, redirect, render_template, request, url_for
 
-
-## Im testing merging. Adding this comment to merge.
-
-
 app = Flask(__name__)
 openai.api_key = "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"
-
-def generate_prompt(input):
-    return """{}
-    This is the user's response and answer to these questions.
-    How would you recommend reducing their emissions?
-    """.format(input.capitalize())
 
 @app.route("/")
 def index():
@@ -22,21 +12,15 @@ def index():
 @app.route("/thisIsTheNameOfRedirect", methods=("GET", "POST"))
 def question():
     if request.method == "POST": 
-        variableQ1 = request.form.get("Do you eat meat?")
-        variableQ2 = request.form.get("Do you use public transport?")
-        variableQ3 =request.form.get("monthly_electric_bill")
-        if variableQ1.lower() in ["yes", "y", "1", "true"]:
-           eat_meat="I eat meat"
-        else:
-            eat_meat="I dont meat"
-        # add more vars here
+        form_values = request.form.items()
+        response_string = ", ".join([f"{k}: {v}" for k, v in form_values])
 
-        fString = f'{eat_meat} Do you use public transport? {variableQ2}. My monthly electric bill is {variableQ3}$ per month'
+        prompt = f'{response_string}. How would you recommend reducing your emissions?'
 
         response = openai.Completion.create(
              model="text-davinci-003",
-             prompt=generate_prompt(fString),
-             temperature=1,
+             prompt=prompt;
+             temperature=0.5,
               max_tokens=500
         )
         
